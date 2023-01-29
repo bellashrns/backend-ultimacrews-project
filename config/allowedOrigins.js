a76536ab76446653900f185058efe7043302df36
bellashrns <bella.saharani@student.umn.ac.id>
2023-01-29 21:19:05 +0700

    add allowed origins 2

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
     'http://localhost:3000',
+    'http://localhost:4173/',
     'https://backend-ultimacrews-project.vercel.app',
     'https://ultimacrews-project.vercel.app'
 ]
