9fa9ada2efa3179a36f967def6ccae774ca8e5fc
bellashrns <bella.saharani@student.umn.ac.id>
2023-01-30 21:25:52 +0700

    hapus !req.session.userId

--- middleware/AuthUser.js
+++ middleware/AuthUser.js
@@ -1,9 +1,9 @@
 import User from "../models/UserModel.js";
 
 export const verifyUser = async(req, res, next) => {
-    if(!req.session.userId){
-        return res.status(401).json({msg: "Mohon login ke akun anda!"});
-    } 
+    // if(!req.session.userId){
+    //     return res.status(401).json({msg: "Mohon login ke akun anda!"});
+    // } 
     const user = await User.findOne({
         where: {
             uuid:req.session.userId
