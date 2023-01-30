d9b8770973fa01513369d74885cb62fb1a9da0ed
bellashrns <bella.saharani@student.umn.ac.id>
2023-01-30 16:16:35 +0700

    //

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'http://localhost:4173/',
-    'http://localhost:5173/',
+    'http://localhost:4173',
+    'http://localhost:5173',
     'https://backend-ultimacrews-project.vercel.app',
     'https://ultimacrews.fun'
 ]
