edcc9b8a454faf22ac81d6ef7371797ea4299b58
bellashrns <bella.saharani@student.umn.ac.id>
2023-01-30 23:36:19 +0700

    ubah order

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -8,7 +8,7 @@ export const login = async(req,res)=>{
         }
     });
     if(!user) return res.status(404).json({msg: "User tidak ditemukan!"});
-    const match = await argon2.verify(req.body.password, user.password);
+    const match = await argon2.verify(user.password, req.body.password);
     if(!match) return res.status(400).json({msg: "Wrong Password!"});
     req.session.userId = user.id;
     const id = user.id;
