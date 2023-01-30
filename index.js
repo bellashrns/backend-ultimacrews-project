017f741e85b2da300125902a4d63d2036a478a08
bellashrns <bella.saharani@student.umn.ac.id>
2023-01-30 20:16:21 +0700

    index

--- index.js
+++ index.js
@@ -37,20 +37,6 @@ app.use(
   })
 );
 
-// app.use((req, res, next) => {
-//   const origin = req.get('referer');
-//   const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
-//   if (isWhitelisted) {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//   }
-//   // Pass to next layer of middleware
-//   if (req.method === 'OPTIONS') res.sendStatus(200);
-//   else next();
-// });
-
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(fileUpload());
