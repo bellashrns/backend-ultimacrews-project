92a7d78a7c27c030db9fc66b8e58eb3c1e0f7070
bellashrns <bella.saharani@student.umn.ac.id>
2023-02-01 00:13:18 +0700

    berhasil lagi

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(
     saveUninitialized: true,
     cookie: {
       // secure: "true",
-      // sameSite: 'strict',
+      sameSite: 'Lax',
       maxAge: 1000 * 60 * 60 * 24,
     },
     store: new MongoStore({
