d18b4d2c20c1e8f6f17e0568811579bd4748121c
bellashrns <bella.saharani@student.umn.ac.id>
2023-01-31 12:44:02 +0700

    wrong password done

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,28 +1,26 @@
-import User from "../models/UserModel.js";
-import bcrypt from "bcrypt";
+import UserModel from "../models/UserModel.js";
+import argon2 from "argon2";
 
 export const login = async(req,res)=>{
-    const user = await User.findOne({
-        where: {
-            email:req.body.email
-        }
+    const user = await UserModel.findOne({
+        email:req.body.email
     });
     if(!user) return res.status(404).json({msg: "User tidak ditemukan!"});
-    const match = await bcrypt.compare(req.body.password, user.password);
+    const match = await argon2.verify(user.password, req.body.password);
     if(!match) return res.status(400).json({msg: "Wrong Password!"});
-    req.session.userId = user.id;
-    const id = user.id;
+    // req.session.userId = user.id;
+    const _id = user._id;
     const username = user.username;
     const email = user.email;
     const role = user.role;
-    res.status(200).json([id, username, email, role]);
+    res.status(200).json([_id, username, email, role]);
 }
 
 export const Me = async(req,res)=>{
     // if(!req.session.userId){
     //     return res.status(401).json({msg: "Mohon login ke akun anda!"});
     // } 
-    const user = await User.findOne({
+    const user = await UserModel.findOne({
         attributes:['id','username','email','role'],
         where: {
             id:req.session.userId
