94f018cdf7c39599cc06ebb70bcbf3eddf49f809
bellashrns <bella.saharani@student.umn.ac.id>
2023-02-02 00:25:05 +0700

    revisi path di multer

--- middleware/Upload.js
+++ middleware/Upload.js
@@ -3,7 +3,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "public/images");
+        cb(null, "/public/images");
     },
     filename: (req, file, cb) => {
         let ext = path.extname(file.originalname);
