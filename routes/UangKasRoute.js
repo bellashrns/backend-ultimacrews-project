6619610eba5f0a5e7565430f74c38e9a40a139ef
Kesya Febriana Manampiring <kesya.febriana@student.umn.ac.id>
2023-02-06 00:06:38 +0700

    Bring back verifyUser

--- routes/UangKasRoute.js
+++ routes/UangKasRoute.js
@@ -11,20 +11,20 @@ import upload from "../middleware/Upload.js";
 
 const router = express.Router();
 
-//What it supposed to be:
-// router.get('/uangkas', verifyUser , getUangKas);
-// router.get('/uangkas/:id', verifyUser, getUangKasById);
-// router.post('/uangkas', upload.single('image'), createUangkas);
-// // router.post('/uangkas', upload.single('image'), verifyUser, createUangkas);
-// router.patch('/uangkas/:id', verifyUser, updateUangKas);
-// router.delete('/uangkas/:id', verifyUser , adminOnly, deleteUangKas);
-
-//For trial & error:
-router.get('/uangkas', getUangKas);
-router.get('/uangkas/:id', getUangKasById);
+// What it supposed to be:
+router.get('/uangkas', verifyUser , getUangKas);
+router.get('/uangkas/:id', verifyUser, getUangKasById);
 router.post('/uangkas', upload.single('image'), createUangkas);
 // router.post('/uangkas', upload.single('image'), verifyUser, createUangkas);
-router.patch('/uangkas/:id', updateUangKas);
-router.delete('/uangkas/:id', deleteUangKas);
+router.patch('/uangkas/:id', bendaharaOnly, updateUangKas);
+router.delete('/uangkas/:id', verifyUser , adminOnly, deleteUangKas);
+
+// //For trial & error:
+// router.get('/uangkas', getUangKas);
+// router.get('/uangkas/:id', getUangKasById);
+// router.post('/uangkas', upload.single('image'), createUangkas);
+// // router.post('/uangkas', upload.single('image'), verifyUser, createUangkas);
+// router.patch('/uangkas/:id', updateUangKas);
+// router.delete('/uangkas/:id', deleteUangKas);
 
 export default router;
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -11,17 +11,17 @@ import { verifyUser, adminOnly } from "../middleware/AuthUser.js";
 const router = express.Router();
 
 // Supposed to be:
-// router.get('/users', verifyUser, getUsers);
-// router.get('/users/:id', verifyUser, getUserById);
-// router.post('/users', verifyUser, adminOnly, createUser);
-// router.patch('/users/:id', verifyUser, updatePassword);
-// router.delete('/users/:id', verifyUser, adminOnly, deleteUser);
+router.get('/users', verifyUser, getUsers);
+router.get('/users/:id', verifyUser, getUserById);
+router.post('/users', verifyUser, adminOnly, createUser);
+router.patch('/users/:id', verifyUser, updatePassword);
+router.delete('/users/:id', verifyUser, adminOnly, deleteUser);
 
-// For Trial & Error:
-router.get('/users', getUsers);
-router.get('/users/:id', getUserById);
-router.post('/users', createUser);
-router.patch('/users/:id', updatePassword);
-router.delete('/users/:id', deleteUser);
+// // For Trial & Error:
+// router.get('/users', getUsers);
+// router.get('/users/:id', getUserById);
+// router.post('/users', createUser);
+// router.patch('/users/:id', updatePassword);
+// router.delete('/users/:id', deleteUser);
 
 export default router;
