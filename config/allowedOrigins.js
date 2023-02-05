5197a1b38d66e5be689c95f94deaa756044c1614
Kesya Febriana Manampiring <kesya.febriana@student.umn.ac.id>
2023-02-06 00:21:12 +0700

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:4173',
     'http://localhost:5173',
-    'https://backend-ultimacrews-project.vercel.app',
+    'http://206.189.153.246:5000',
     'https://ultimacrews.fun'
 ]
 
